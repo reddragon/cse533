e0238095897fde92c9c586a368304e0e103ff19b
Dhruv <dhruvbird@gmail.com>
2012-11-11 16:38:26 -0500

    IP address are only 16 bytes including the trailing NULL character (end of string).

--- hw3/api.h
+++ hw3/api.h
@@ -8,11 +8,11 @@ typedef enum request_type {
 
 // The message which will be sent from the API to the ODR, or the other way round
 #define API_MSG_SZ 256
-#define API_MSG_HDR_SZ (sizeof(request_type) + sizeof(char)*20 + sizeof(int)*2)
+#define API_MSG_HDR_SZ (sizeof(request_type) + sizeof(char)*16 + sizeof(int)*2)
 typedef struct api_msg {
   request_type rtype;
   int port;
-  char ip[20];
+  char ip[16];
   int flag;
   char msg[API_MSG_SZ - API_MSG_HDR_SZ];
 } api_msg;
