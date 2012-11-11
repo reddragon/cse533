6864ff76a275305091fbf95c05d557c4653a86ad
Gaurav Menghani <gaurav.menghani@gmail.com>
2012-11-11 12:19:53 -0800

    Added VERBOSE statements before and after recv

--- hw3/api.c
+++ hw3/api.c
@@ -10,7 +10,7 @@ msg_send(int sockfd, char *dst_ip, int dst_port, char *msg, int flag) {
   strcpy(m.ip, dst_ip);
   m.flag = flag;
   strncpy(m.msg, msg, sizeof(api_msg) - API_MSG_HDR_SZ);
-
+  
   // Send the newly marshalled API message to the ODR.
   // The ODR will take care of the rest
   Send(sockfd, (char *) &m, sizeof(api_msg), 0);
@@ -23,15 +23,16 @@ msg_recv(int sockfd, char *src_ip, int *src_port, char *msg) {
   api_msg m;
   bzero(&m, sizeof(api_msg));
   m.rtype = MSG_RECV;
-
-  assert(((api_msg*)(0))->msg == API_MSG_HDR_SZ);
+  
   // Just send the header of the API Message
   Send(sockfd, (char *) &m, API_MSG_HDR_SZ, 0);
   
   // TODO Put a timeout here
   // The message which will have the response
   api_msg r;
+  VERBOSE("Waiting for a Recv\n%s", "");
   Recv(sockfd, (char *) &r, sizeof(api_msg), 0);
+  VERBOSE("Came out of a Recv\n%s", "");
   *src_port = r.port;
   strncpy(src_ip, r.ip, 20);
   strncpy(msg, r.msg, sizeof(r.msg));
