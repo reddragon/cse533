0e2dac165bdd26575d3b6bae74af91cacc4b2ba3
Dhruv <dhruvbird@gmail.com>
2012-11-20 13:36:57 -0500

    Updated post.py.

--- hw3/post.py
+++ hw3/post.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python -u
+
 import os
 import sys
-print "POST /%s HTTP/1.1\r\nContent-Length: 10000000\r\n\r\n" % os.environ['HOSTNAME']
+
+if len(sys.argv) < 3:
+        print "Usage: %s <etype> <executable> <args...>" % (sys.argv[0])
+        sys.exit(1)
+
+etype      = sys.argv[1]
+executable = sys.argv[2]
+args       = sys.argv[3:]
+
+print "POST /%s_%s HTTP/1.1\r\nContent-Length: 10000000\r\n\r\n" % (etype, os.environ['HOSTNAME'])
 sys.stdout.flush()
-p = os.popen("./ODR_gmenghani " + sys.argv[1], 'r', 0)
+p = os.popen("%s " % (executable, " ".join(args)), 'r', 0)
 line = p.readline()
+
 while line != '':
 	sys.stdout.write(line)
 	line = p.readline()
