80fb0a93f57327dc92adbe54d83cd022ebf97f28
Dhruv <dhruvbird@gmail.com>
2012-11-26 19:46:29 -0500

    Print hostname on server as well.

--- hw3/timeserv.c
+++ hw3/timeserv.c
@@ -10,6 +10,8 @@
 
 serv_dsock s;                  // server's domain socket
 fdset fds;                     // fdset for the server's domain socket
+char my_hostname[200];         // My hostname (i.e. hostname of IP bound to eth0)
+char my_ipaddr[16];            // My IP Address
 
 void on_server_exit(void) {
   struct timeval tv;
@@ -31,6 +33,7 @@ void on_recv(void *opaque) {
   // Received a message.
   int r;
   char src_ip[20];
+  char src_hostname[200];
   int src_port;
   char msg[30];
   time_t rawtime;
@@ -49,7 +52,12 @@ void on_recv(void *opaque) {
     }
   }
 
-  INFO("Received message: '%s' from %s:%d\n", msg, src_ip, src_port);
+  src_hostname[0] = '\0';
+  ip_address_to_hostname(src_ip, src_hostname);
+
+  INFO("server at node %s responding to request from %s:%d\n",
+       my_hostname, src_hostname, src_port);
+
   VERBOSE("Sending a message to the client %s:%d\n", src_ip, src_port);
   strftime(msg, sizeof(msg), "%c", timeinfo);
   r = msg_send(s.sockfd, src_ip, src_port, msg, 0);
@@ -91,11 +99,30 @@ void on_interrupt(int status) {
   exit(0);
 }
 
+void server_setup(void) {
+  struct hwa_info *h;
+  struct hwa_info *h_head;
+  struct sockaddr *sa;
+
+  h_head = Get_hw_addrs();
+  for (h = h_head; h != NULL; h = h->hwa_next) {
+    if (!strcmp(h->if_name, "eth0") && h->ip_addr != NULL) {
+      sa = h->ip_addr;
+      strcpy(my_ipaddr, (char *)Sock_ntop_host(sa, sizeof(*sa)));
+      my_hostname[0] = '\0';
+      ip_address_to_hostname(my_ipaddr, my_hostname);
+      INFO("My IP Address: %s & hostname: %s\n", my_ipaddr, my_hostname);
+      break;
+    }
+  }
+}
+
 int
 main(int argc, char **argv) {
   assert(((api_msg*)(0))->msg == (void *)API_MSG_HDR_SZ);
   atexit(on_server_exit);
 
+  server_setup();
   memset(&s, 0, sizeof(s));
   create_srv_dsock(&s);
 
