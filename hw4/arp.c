f53eacdd07b7e089d72a88357e854e544180f9ef
Gaurav Menghani <gaurav.menghani@gmail.com>
2012-12-04 16:23:08 -0800

    Added some code to the ARP part

--- hw4/arp.c
+++ hw4/arp.c
@@ -11,7 +11,57 @@ typedef struct addr_pair {
   char              if_name[10];
 } addr_pair;
 
+// ARP Cache Entry
+typedef struct cache_entry {
+  ipaddr_n    ip_n;
+  eth_addr_n  eth_n;
+  int         sll_ifindex;
+  int         sll_hatype;
+  int         sockfd;
+} cache_entry;
+
+#define ETHERNET_PAYLOAD_SIZE 120
+
+typedef struct eth_frame {
+  eth_addr_n dst_eth_addr;  // Destination Ethernet Address
+  eth_addr_n src_eth_addr;  // Source Ethernet Address
+  uint16_t protocol;        // Protocol
+  char payload[ETHERNET_PAYLOAD_SIZE];       // Payload
+} eth_frame;
+
+typedef struct arp_pkt {
+  uint16_t    hard_type;
+  uint16_t    prot_type;
+  uint8_t     hard_size;
+  uint8_t     prot_size;
+  uint16_t    op;
+  eth_addr_n  sender_eth_addr;
+  ipaddr_n;   sender_ip_addr;
+  eth_addr_n  target_eth_addr;
+  ipaddr_n;   target_ip_addr;
+} arp_pkt;
+
+
 vector addr_pairs;
+
+// This is the protocol number for ARP packets (used in the eth frames)
+#define ARP_PROTOCOL 0x806 
+
+eth_frame*
+create_arp_request(eth_addr_n dst_addr) {
+  eth_frame *ef;
+  arp_pkt pkt;
+  ef->dst_addr = dst_addr;
+  ef->src_addr = ((addr_pair *)vector_at(&addr_pairs, 0))->eth_n;
+  ef->protocol = ARP_PROTOCOL;
+  ef = MALLOC(eth_frame); 
+  
+
+  memcpy(ef->payload, pkt, sizeof(arp_pkt));
+
+}
+
+
 
 void
 get_addr_pairs(void) {
@@ -35,6 +85,8 @@ get_addr_pairs(void) {
       vector_push_back(&addr_pairs, a);
     }
   }
+  // Checking if we got atleast one Eth-Addr, IP-Addr pair.
+  ASSERT(vector_size(&addr_pairs) > 0);
 }
 
 void
@@ -44,6 +96,7 @@ arp_setup(void) {
 
 int 
 main(int argc, char **argv) {
+  assert(sizeof(arp_pkt) == 28);
   arp_setup();
   return 0;
 }
