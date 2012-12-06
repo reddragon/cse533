ee9cbe7ef028ad8d1023567bc98b39a7150c8432
Dhruv <dhruvbird@gmail.com>
2012-12-06 18:15:31 -0500

    Some changes to tour.c.

--- hw4/tour.c
+++ hw4/tour.c
@@ -4,21 +4,40 @@
 #include "unp.h"
 #include <linux/if_ether.h>
 
-int rt, pg, pf, udp;
-vector tour_hosts;  // List of hostnames that we have to visit, excluding us.
-ipaddr_ascii myip;  // TODO Populate this
-tour_list tour;     // TODO Populate this (also add myip to the list)
-bool visited;       // Whether this node has been touched by a tour
+int rt, pg, pf, udp; // rt -> Routing; pg -> Ping; pf -> PF Packet; udp -> For Multicast
+vector tour_hosts;   // List of hostnames that we have to visit, excluding us.
+ipaddr_ascii myip_a; // My IP address in presentation format
+ipaddr_n     myip_n; // My IP address in network byte order
+tour_list tour;      // List of IP addresses (in network
+                     // order). Includes my own IP address.
+bool visited;        // Whether this node has been touched by a tour
+fdset fds;           // List of FDs to wait on
 
-void tour_setup(void) {
+struct hostname_t {
+  char hostname[60];
+} hostname_t;
+
+void tour_setup(int argc, char *argv[]) {
   const int yes = 1;
+  int i;
   rt = Socket(AF_INET, SOCK_RAW, IPPROTO_HW);
   Setsockopt(rt, SOL_SOCKET, IP_HDRINCL, &yes, sizeof(yes));
   pg = Socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
   pf = Socket(PF_PACKET, SOCK_RAW, ETH_P_IP);
   udp = Socket(AF_INET, SOCK_DGRAM, 0); // FIXME Fix the protocol
   
-  vector_init(&tour_hosts, sizeof(char *));
+  vector_init(&tour_hosts, sizeof(hostname_t));
+  hostname_t hn;
+
+  // TODO: Get my eth0 IP address.
+  strcpy(hn.hostname, myip);
+
+  vector_push_back(&tour_hosts, &hn);
+
+  for (i = 1; i < argc; i++) {
+    strcpy(hn.hostname, argv[i]);
+    vector_push_back(&tour_hosts, &hn);
+  }
 }
 
 void
@@ -38,11 +57,4 @@ act_on_pkt(ip_pkt *pkt) {
 int
 main(int argc, char **argv) {
   int i;
-  if (argc > 1) {
-    // TODO Push your own host-name
-    for (i = 1; i < argc; i++) {
-      vector_push_back(&tour_hosts, argv[1]);
-    }
-  } else {
-  }
 }
